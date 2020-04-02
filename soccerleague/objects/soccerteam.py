96c5524212e324734bd8cb173880f76a0fd7a7e2
Brendan <brendanbath@gmail.com>
2020-04-02 21:09:39 +0200

    Updated docstrings

--- soccerleague/objects/soccerteam.py
+++ soccerleague/objects/soccerteam.py
@@ -1,10 +1,9 @@
 class SoccerTeam:
+    """ An object which represents a soccer team
     """
-    Attributes
-    ----------
-    team_name : str
-        Name of the team which the object is describing
-    """
+
+    __team_name: str = None
+
     def __init__(self, team_name: str):
         """Init of the SoccerTeam class
 
@@ -18,16 +17,17 @@ class SoccerTeam:
         self.team_name = team_name
 
     @property
-    def team_name(self):
+    def team_name(self) -> str:
+        """team_name: str object which represents the team's name
+
+        The setter checks if the correct type str is being set else it raises a TypeError
+        """
         return self.__team_name
 
     @team_name.setter
-    def team_name(self, team_name):
+    def team_name(self, team_name: str):
         if type(team_name) is str:
             self.__team_name = team_name
         else:
-            raise TypeError
+            raise TypeError('Team name needs to be of type str')
 
-    @team_name.getter
-    def team_name(self):
-        return self.__team_name
